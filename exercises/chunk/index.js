d5b5cfcb6f5d8f4b619128fb97b655447d1c5187
merjench <merjen.chary@gmail.com>
2019-08-01 13:44:16 -0400

    chunk solution 1

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,10 +10,21 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 
-
+//--------------Solution 1 ----------------///
 
 function chunk(array, size) {
+  const chunked = [];
 
+  for ( let element of array) {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element)
+    }
+  }
+   return chunked
 }
 
 module.exports = chunk;
