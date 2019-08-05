8831b35492816e945056e9261b69c209a1b2f8f7
merjench <merjen.chary@gmail.com>
2019-08-05 16:17:27 -0400

    meandering_array solution and repl testing

--- exercises/meandering_array/index.js
+++ exercises/meandering_array/index.js
@@ -1,28 +1,48 @@
 'use strict';
 
-const fs = require('fs');
+// const fs = require('fs');
+//
+// process.stdin.resume();
+// process.stdin.setEncoding('utf-8');
+//
+// let inputString = '';
+// let currentLine = 0;
+//
+// process.stdin.on('data', function(inputStdin) {
+//     inputString += inputStdin;
+// });
+//
+// process.stdin.on('end', function() {
+//     inputString = inputString.split('\n');
+//
+//     main();
+// });
+//
+// function readLine() {
+//     return inputString[currentLine++];
+// }
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+//-----------Repl Testing --------------------------------////////////////
 
-let inputString = '';
-let currentLine = 0;
+var array = [5,2,7,8,-2,25,25], 
+  newArray = [];
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
+for(var i=0; i < array.length; i++) {
+    var max = Math.max.apply(null, array), min = Math.min.apply(null, array);
+    array.splice(array.indexOf(max), 1);
+    array.splice(array.indexOf(min), 1);
+    newArray.push(max, min);
 }
+if (array.length) newArray.push(array.pop());
+
+console.log(newArray);
+
+///-------------------------------------------------------------------
 
 
+
+
+/////////---------------------Solution------------------------////////
 /*
  * Complete the 'meanderingArray' function below.
  *
@@ -43,24 +63,26 @@ function readLine() {
     if (unsorted.length) newArray.push(unsorted.pop());
 
         return newArray
-}
+//---------------------------------------------------------------------------------------
 
 
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const unsortedCount = parseInt(readLine().trim(), 10);
-
-    let unsorted = [];
-
-    for (let i = 0; i < unsortedCount; i++) {
-        const unsortedItem = parseInt(readLine().trim(), 10);
-        unsorted.push(unsortedItem);
-    }
-
-    const result = meanderingArray(unsorted);
-
-    ws.write(result.join('\n') + '\n');
-
-    ws.end();
-}
+//
+// function main() {
+//     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+//
+//     const unsortedCount = parseInt(readLine().trim(), 10);
+//
+//     let unsorted = [];
+//
+//     for (let i = 0; i < unsortedCount; i++) {
+//         const unsortedItem = parseInt(readLine().trim(), 10);
+//         unsorted.push(unsortedItem);
+//     }
+//
+//     const result = meanderingArray(unsorted);
+//
+//     ws.write(result.join('\n') + '\n');
+//
+//     ws.end();
+// }
