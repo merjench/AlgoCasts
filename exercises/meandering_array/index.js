5e4284834d5cd3d01ffb27dd90102262864a08e8
merjench <merjen.chary@gmail.com>
2019-08-05 16:14:15 -0400

    meandering_array done

---
+++ exercises/meandering_array/index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+/*
+ * Complete the 'meanderingArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY unsorted as parameter.
+ */
+
+ function meanderingArray(unsorted) {
+    // Write your code here
+    var newArray = [];
+    while(unsorted.length > 1) {
+        var max = Math.max.apply(null, unsorted), min = Math.min.apply(null, unsorted);
+        unsorted.splice(unsorted.indexOf(max), 1);
+        unsorted.splice(unsorted.indexOf(min), 1);
+        newArray.push(max, min);
+    }
+
+    if (unsorted.length) newArray.push(unsorted.pop());
+
+        return newArray
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const unsortedCount = parseInt(readLine().trim(), 10);
+
+    let unsorted = [];
+
+    for (let i = 0; i < unsortedCount; i++) {
+        const unsortedItem = parseInt(readLine().trim(), 10);
+        unsorted.push(unsortedItem);
+    }
+
+    const result = meanderingArray(unsorted);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
