38cd9df66bfa302b8516dee4f84b8b299c3e81b8
merjench <merjen.chary@gmail.com>
2019-09-18 19:23:13 -0400

    stock price change

---
+++ exercises/stocks/index.js
@@ -0,0 +1,8 @@
+// Problem:
+// The prediction of a particular stock for a period is given to you
+// and you want to maximize the profit of that stock by buying it once and selling it once in tht period.
+
+
+// Solution 1:
+
+array = [4, 9, 1, 10, 13, 0, 10]
